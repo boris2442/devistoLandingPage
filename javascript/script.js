b03b4030fe1d9d965d13775bf4159c9dd89001ad
boris2442 <aubinborissimotsebo@gmail.com>
2025-01-20 16:35:41 +0100

    menu burger javascript

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,15 @@
 const menu=document.querySelector(".bars");
 const navigation=document.querySelector("header ul");
-
+const remove=document.querySelector(".fa-x")
 menu.addEventListener("click", ()=>{
-    navigation.classList.toggle("navigationHover");
+    navigation.classList.add("navigationHover");
+    menu.style.display='none';
+    remove.style.opacity='1'
+
 });
+remove.addEventListener("click", ()=>{
+    navigation.classList.remove("navigationHover");   
+    menu.style.display='';
+     remove.style.opacity='0'
+}
+)
