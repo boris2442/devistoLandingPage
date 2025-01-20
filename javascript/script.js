9e2a12df931d49820728aee553089577008652d6
boris2442 <aubinborissimotsebo@gmail.com>
2025-01-20 16:41:13 +0100

    text

--- javascript/script.js
+++ javascript/script.js
@@ -5,11 +5,13 @@ menu.addEventListener("click", ()=>{
     navigation.classList.add("navigationHover");
     menu.style.display='none';
     remove.style.opacity='1'
+    remove.classList.add("remove2")
 
 });
 remove.addEventListener("click", ()=>{
     navigation.classList.remove("navigationHover");   
     menu.style.display='';
      remove.style.opacity='0'
+     remove.classList.remove("remove2")
 }
 )
